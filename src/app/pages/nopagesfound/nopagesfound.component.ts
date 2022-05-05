d48165b0605a66ddc7178be976a9d34888d5803f
andres <andres@mediabyte.com.pe>
2022-05-05 17:34:21 -0500

    diseño

---
+++ src/app/pages/nopagesfound/nopagesfound.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagesfound',
+  templateUrl: './nopagesfound.component.html',
+  styleUrls: ['./nopagefound.component.scss']
+})
+export class NopagesfoundComponent {
+
+  year = new Date().getFullYear();
+
+  
+
+}
